c34bf901160871efe003991599e6679f9bfc3e67
Bryan J. Brown <bjbrown@fsu.edu>
2019-03-02 11:18:10 -0500

    Brings Drupal 8 up and resets HTTPD

--- build_scripts/drupal.sh
+++ build_scripts/drupal.sh
@@ -11,26 +11,63 @@ echo "DrupalAdminEmail: ${DRUPAL_ADMIN_EMAIL}" >> /root/drupal-build-params.txt
 
 
 # Run updates & installations
-apt -y install apache2 unzip php php-dev php-gd php-soap php-mysql php-mbstring php-zip composer > /root/drupal-dependencies.txt 2>&1
+apt -y install apache2 unzip \
+	php php-dev php-gd php-soap php-mysql php-mbstring php-zip php-curl composer \
+	> /root/drupal-dependencies.txt 2>&1
 
 
 # Configure MySQL
-mysql --user="${DATABASE_ROOT_USER}" --password="${DATABASE_ROOT_PASS}" --host="${DATABASE_ENDPOINT}" --execute="CREATE DATABASE ldbasedb;" >> /root/mysql-setup.txt 2>&1
-mysql --user="${DATABASE_ROOT_USER}" --password="${DATABASE_ROOT_PASS}" --host="${DATABASE_ENDPOINT}" --execute="CREATE USER '${DRUPAL_DATABASE_USER}'@'%' IDENTIFIED BY '${DRUPAL_DATABASE_PASS}';" >> /root/mysql-setup.txt 2>&1
-mysql --user="${DATABASE_ROOT_USER}" --password="${DATABASE_ROOT_PASS}" --host="${DATABASE_ENDPOINT}" --execute="GRANT ALL PRIVILEGES ON ldbasedb.* TO '${DRUPAL_DATABASE_USER}'@'%';" >> /root/mysql-setup.txt 2>&1
-mysql --user="${DATABASE_ROOT_USER}" --password="${DATABASE_ROOT_PASS}" --host="${DATABASE_ENDPOINT}" --execute="FLUSH PRIVILEGES;" >> /root/mysql-setup.txt 2>&1
+mysql --user="${DATABASE_ROOT_USER}" \
+	--password="${DATABASE_ROOT_PASS}" \
+	--host="${DATABASE_ENDPOINT}" \
+	--execute="CREATE DATABASE ldbasedb;" \
+	>> /root/mysql-setup.txt 2>&1
+mysql --user="${DATABASE_ROOT_USER}" \
+	--password="${DATABASE_ROOT_PASS}" \
+	--host="${DATABASE_ENDPOINT}" \
+	--execute="CREATE USER '${DRUPAL_DATABASE_USER}'@'%' IDENTIFIED BY '${DRUPAL_DATABASE_PASS}';" \
+	>> /root/mysql-setup.txt 2>&1
+mysql --user="${DATABASE_ROOT_USER}" \
+	--password="${DATABASE_ROOT_PASS}" \
+	--host="${DATABASE_ENDPOINT}" \
+	--execute="GRANT ALL PRIVILEGES ON ldbasedb.* TO '${DRUPAL_DATABASE_USER}'@'%';" \
+	>> /root/mysql-setup.txt 2>&1
+mysql --user="${DATABASE_ROOT_USER}" \
+	--password="${DATABASE_ROOT_PASS}" \
+	--host="${DATABASE_ENDPOINT}" \
+	--execute="FLUSH PRIVILEGES;" \
+	>> /root/mysql-setup.txt 2>&1
 
 
-# Configure Drupal
+# Install and configure Drupal
+cd /var/www/html/
+rm index.html
+composer global require zaporylie/composer-drupal-optimizations
+composer create-project drupal-composer/drupal-project:8.x-dev drupal --stability dev --no-interaction
+echo "PATH=/var/www/html/drupal/vendor/bin/:$PATH" >> /root/.bashrc
+echo "PATH=/var/www/html/drupal/vendor/bin/:$PATH" >> /home/vagrant/.bashrc
+. /root/.bashrc
+cd /var/www/html/drupal
+drupal site:install standard \
+	--langcode="en" \
+	--db-type="mysql" \
+	--db-host="${DATABASE_ENDPOINT}" \
+	--db-name="ldbasedb" \
+	--db-user="${DRUPAL_DATABASE_USER}" \
+	--db-pass="${DRUPAL_DATABASE_PASS}" \
+	--db-port="3306" \
+	--site-name="LDbase" \
+	--site-mail="${DRUPAL_ADMIN_EMAIL}" \
+	--account-name="${DRUPAL_ADMIN_USER}" \
+	--account-pass="${DRUPAL_ADMIN_PASS}" \
+	--account-mail="${DRUPAL_ADMIN_EMAIL}" \
+	--no-interaction
+drupal cache:rebuild
 
 
 # Configure Apache 
 echo "AddHandler php5-script .php" >> /etc/apache2/apache2.conf
 echo "AddType text/html .php" >> /etc/apache2/apache2.conf
 sed -i -e 's/AllowOverride\ None/AllowOverride\ All/g' /etc/apache2/apache2.conf
-service apache2 restart
-
-
-# Final refresh of system before exiting
-# drush cache-rebuild
+sed -i -e 's/\/var\/www\/html/\/var\/www\/html\/drupal\/web/g' /etc/apache2/sites-available/000-default.conf
 service apache2 restart
