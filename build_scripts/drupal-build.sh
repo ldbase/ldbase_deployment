7e8513b1323302ac11a522ab3655ca2071d270d0
Bryan J. Brown <bjbrown@fsu.edu>
2019-06-28 11:54:59 -0400

    Updates deployment to use separate config repo instead of installation profile

--- build_scripts/drupal-build.sh
+++ build_scripts/drupal-build.sh
@@ -83,11 +83,10 @@ drupal site:install standard \
 	--no-interaction \
         >> /root/drupal-installation.txt 2>&1
 
+
 # Configure Apache 
 echo "AddHandler php5-script .php" >> /etc/apache2/apache2.conf
 echo "AddType text/html .php" >> /etc/apache2/apache2.conf
 sed -i -e 's/AllowOverride\ None/AllowOverride\ All/g' /etc/apache2/apache2.conf
 sed -i -e 's/\/var\/www\/html/\/var\/www\/html\/drupal\/web/g' /etc/apache2/sites-available/000-default.conf
-
-
 service apache2 restart
--- build_scripts/drupal-config.sh
+++ build_scripts/drupal-config.sh
@@ -16,6 +16,7 @@ fi
 echo '];' \
 	>> /var/www/html/drupal/web/sites/default/settings.php
 
+
 # Pre-install missing dependencies
 if [ -d /vagrant ]; then service mysql stop; fi
 cd /var/www/html/drupal
@@ -34,7 +35,6 @@ composer require drupal/field_group >> /root/composer-preinstalls.txt 2>&1
 composer require drupal/crop >> /root/composer-preinstalls.txt 2>&1
 composer require drupal/image_widget_crop >> /root/composer-preinstalls.txt 2>&1
 composer require drupal/better_exposed_filters >> /root/composer-preinstalls.txt 2>&1
-composer require drupal/entity_reference_revisions >> /root/composer-preinstalls.txt 2>&1
 composer require drupal/fontawesome >> /root/composer-preinstalls.txt 2>&1
 composer require drupal/fontyourface >> /root/composer-preinstalls.txt 2>&1
 composer require drupal/juicerio >> /root/composer-preinstalls.txt 2>&1
@@ -45,18 +45,65 @@ composer require drupal/video_embed_field >> /root/composer-preinstalls.txt 2>&1
 composer require drupal/views_infinite_scroll >> /root/composer-preinstalls.txt 2>&1
 composer require drupal/inline_entity_form >> /root/composer-preinstalls.txt 2>&1
 composer require drupal/paragraphs >> /root/composer-preinstalls.txt 2>&1
+composer require drupal/entity_reference_revisions >> /root/composer-preinstalls.txt 2>&1
 composer require drupal/entity_usage >> /root/composer-preinstalls.txt 2>&1
 composer require drupal/rules >> /root/composer-preinstalls.txt 2>&1
 composer require drupal/group >> /root/composer-preinstalls.txt 2>&1
 composer require drupal/matomo >> /root/composer-preinstalls.txt 2>&1
 if [ -d /vagrant ]; then service mysql start; fi
 
+
+# Enable LDbase required modules
+/var/www/html/drupal/vendor/bin/drupal module:install token >> /root/drupal-module-installation.txt 2>&1
+/var/www/html/drupal/vendor/bin/drupal module:install backup_migrate >> /root/drupal-module-installation.txt 2>&1
+/var/www/html/drupal/vendor/bin/drupal module:install simple_sitemap >> /root/drupal-module-installation.txt 2>&1
+/var/www/html/drupal/vendor/bin/drupal module:install simple_sitemap_views >> /root/drupal-module-installation.txt 2>&1
+/var/www/html/drupal/vendor/bin/drupal module:install metatag >> /root/drupal-module-installation.txt 2>&1
+/var/www/html/drupal/vendor/bin/drupal module:install metatag_views >> /root/drupal-module-installation.txt 2>&1
+/var/www/html/drupal/vendor/bin/drupal module:install metatag_hreflang >> /root/drupal-module-installation.txt 2>&1
+/var/www/html/drupal/vendor/bin/drupal module:install metatag_favicons >> /root/drupal-module-installation.txt 2>&1
+/var/www/html/drupal/vendor/bin/drupal module:install metatag_mobile >> /root/drupal-module-installation.txt 2>&1
+/var/www/html/drupal/vendor/bin/drupal module:install metatag_dc >> /root/drupal-module-installation.txt 2>&1
+/var/www/html/drupal/vendor/bin/drupal module:install metatag_dc_advanced >> /root/drupal-module-installation.txt 2>&1
+/var/www/html/drupal/vendor/bin/drupal module:install metatag_facebook >> /root/drupal-module-installation.txt 2>&1
+/var/www/html/drupal/vendor/bin/drupal module:install metatag_facebook >> /root/drupal-module-installation.txt 2>&1
+/var/www/html/drupal/vendor/bin/drupal module:install metatag_google_cse >> /root/drupal-module-installation.txt 2>&1
+/var/www/html/drupal/vendor/bin/drupal module:install metatag_open_graph >> /root/drupal-module-installation.txt 2>&1
+/var/www/html/drupal/vendor/bin/drupal module:install metatag_pinterest >> /root/drupal-module-installation.txt 2>&1
+/var/www/html/drupal/vendor/bin/drupal module:install metatag_twitter_cards >> /root/drupal-module-installation.txt 2>&1
+/var/www/html/drupal/vendor/bin/drupal module:install devel >> /root/drupal-module-installation.txt 2>&1
+/var/www/html/drupal/vendor/bin/drupal module:install devel_generate >> /root/drupal-module-installation.txt 2>&1
+/var/www/html/drupal/vendor/bin/drupal module:install devel_php >> /root/drupal-module-installation.txt 2>&1
+/var/www/html/drupal/vendor/bin/drupal module:install kint >> /root/drupal-module-installation.txt 2>&1
+/var/www/html/drupal/vendor/bin/drupal module:install webprofiler >> /root/drupal-module-installation.txt 2>&1
+/var/www/html/drupal/vendor/bin/drupal module:install webform >> /root/drupal-module-installation.txt 2>&1
+/var/www/html/drupal/vendor/bin/drupal module:install webform_access >> /root/drupal-module-installation.txt 2>&1
+/var/www/html/drupal/vendor/bin/drupal module:install webform_image_select >> /root/drupal-module-installation.txt 2>&1
+/var/www/html/drupal/vendor/bin/drupal module:install webform_node >> /root/drupal-module-installation.txt 2>&1
+/var/www/html/drupal/vendor/bin/drupal module:install webform_submission_log >> /root/drupal-module-installation.txt 2>&1
+/var/www/html/drupal/vendor/bin/drupal module:install webform_templates >> /root/drupal-module-installation.txt 2>&1
+/var/www/html/drupal/vendor/bin/drupal module:install webform_ui >> /root/drupal-module-installation.txt 2>&1
+/var/www/html/drupal/vendor/bin/drupal module:install webform_views >> /root/drupal-module-installation.txt 2>&1
+/var/www/html/drupal/vendor/bin/drupal module:install honeypot >> /root/drupal-module-installation.txt 2>&1
+/var/www/html/drupal/vendor/bin/drupal module:install search_api >> /root/drupal-module-installation.txt 2>&1
+/var/www/html/drupal/vendor/bin/drupal module:install search_api_db >> /root/drupal-module-installation.txt 2>&1
+/var/www/html/drupal/vendor/bin/drupal module:install search_api_autocomplete >> /root/drupal-module-installation.txt 2>&1
+/var/www/html/drupal/vendor/bin/drupal module:install facets >> /root/drupal-module-installation.txt 2>&1
+/var/www/html/drupal/vendor/bin/drupal module:install entity_usage >> /root/drupal-module-installation.txt 2>&1
+/var/www/html/drupal/vendor/bin/drupal module:install paragraphs >> /root/drupal-module-installation.txt 2>&1
+/var/www/html/drupal/vendor/bin/drupal module:install rules >> /root/drupal-module-installation.txt 2>&1
+/var/www/html/drupal/vendor/bin/drupal module:install group >> /root/drupal-module-installation.txt 2>&1
+/var/www/html/drupal/vendor/bin/drupal module:install gnode >> /root/drupal-module-installation.txt 2>&1
+/var/www/html/drupal/vendor/bin/drupal module:install matomo >> /root/drupal-module-installation.txt 2>&1
+/var/www/html/drupal/vendor/bin/drush webform:libraries:download >> /root/drupal-module-installation.txt 2>&1
+
+
 # Download JavaScript libraries
 cd /var/www/html/drupal/web/libraries
 git clone https://github.com/components/highlightjs
-mkdir d3; cd d3; wget https://d3js.org/d3.v3.min.js; mv d3.v3.min.js d3.min.js
+mkdir d3; cd d3; wget https://d3js.org/d3.v3.min.js; mv d3.v3.min.js d3.min.js >> /dev/null 2>&1
 cd /var/www/html/drupal 
-/var/www/html/drupal/vendor/bin/drush webform:libraries:download
+
 
 # Import LDbase configuration data
 cd /var/www/html/drupal/
@@ -65,6 +112,7 @@ cd /var/www/html/drupal/
 cd /root/
 git clone https://github.com/ldbase/ldbase_config
 /root/ldbase_config/bin/import.sh >> /root/ldbase-system-config.txt 2>&1
+
 
 # Prepare for lift off 
 /var/www/html/drupal/vendor/bin/drupal node:access:rebuild
