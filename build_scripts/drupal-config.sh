d69d4d69341714647d5ba5695758420b7b2df277
Bryan J. Brown <bjbrown@fsu.edu>
2020-03-11 11:42:03 -0400

    Updates location of config dir and adds proto composer.json file

--- build_scripts/drupal-config.sh
+++ build_scripts/drupal-config.sh
@@ -92,7 +92,7 @@ git clone https://github.com/fsulib/portcullis
 cd /var/www/html/drupal/
 /var/www/html/drupal/vendor/bin/drush config-set "system.site" uuid "6d3939a8-a52f-4862-a77a-176786dcad2a" -y >> /root/ldbase-system-config.txt 2>&1
 /var/www/html/drupal/vendor/bin/drush ev '\Drupal::entityManager()->getStorage("shortcut_set")->load("default")->delete();' >> /root/ldbase-system-config.txt 2>&1
-/var/www/html/drupal/vendor/bin/drupal config:import --directory=/var/www/html/drupal/web/modules/custom/ldbase_config >> /root/ldbase-system-config.txt 2>&1
+/var/www/html/drupal/vendor/bin/drupal config:import --directory=/var/www/html/drupal/web/modules/custom/ldbase_config/sync >> /root/ldbase-system-config.txt 2>&1
 /var/www/html/drupal/vendor/bin/drupal ldbase:importTerms >> /root/ldbase-system-config.txt 2>&1
 
 
