a4e2bd7eadb67843a0cf99df744dde7f8b981152
Bryan J. Brown <bjbrown@fsu.edu>
2020-03-25 09:40:56 -0400

    Update ldbase-custom-codebase-update.sh

--- build/ldbase-custom-codebase-update.sh
+++ build/ldbase-custom-codebase-update.sh
@@ -3,8 +3,8 @@ cd /ldbase_deployment; git pull
 
 cd /var/www/html/drupal
 composer update \
-	fsulib/portcullis \
-	ldbase/ldbase_content \
+  fsulib/portcullis \
+  ldbase/ldbase_content \
   ldbase/ldbase_handlers \
   ldbase/ldbase_config \
-	--no-interaction
+  --no-interaction
