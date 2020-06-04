cc0249d11cab71ab7c47ec1e57e86b331ad09971
Bryan J. Brown <bryjbrown@fsu.edu>
2020-06-04 14:15:54 -0400

    Configures MinIO host in preparation for Drupal usage

--- build/minio-install.sh
+++ build/minio-install.sh
@@ -1,5 +1,14 @@
 cd /root/
+
 wget https://dl.min.io/server/minio/release/linux-amd64/minio
 chmod +x minio
 mv minio /usr/local/bin/ 
+
+wget https://dl.min.io/client/mc/release/linux-amd64/mc
+chmod +x mc
+mv mc /usr/local/bin/
+
 minio server /data > /dev/null 2>&1 &
+mc config host add localhost http://localhost:9000 minioadmin minioadmin
+mkdir /data/ldbase
+mc policy set public localhost/ldbase
