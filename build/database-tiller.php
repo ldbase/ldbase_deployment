d22522a0a688979f2b270518c5dededf60e37d3b
Bryan Brown <bryjbrown@gmail.com>
2020-08-21 13:39:51 -0400

    Gets rid of unneeded stale commands

--- build/database-tiller.php
+++ build/database-tiller.php
@@ -13,10 +13,7 @@ $db_end_string = "\" >> /root/drupal-rebuild.txt 2>&1";
 
 shell_exec($db_auth_string . "DROP DATABASE ${db_name};" . $db_end_string);
 shell_exec($db_auth_string . "CREATE DATABASE ${db_name};" . $db_end_string);
-shell_exec($db_auth_string . "CREATE USER '${db_user}'@'%' IDENTIFIED BY '${db_pass}';" . $db_end_string);
-shell_exec($db_auth_string . "GRANT ALL PRIVILEGES ON ${db_name}.* TO '${db_user}'@'%';" . $db_end_string);
-shell_exec($db_auth_string . "FLUSH PRIVILEGES;" . $db_end_string);
 shell_exec('service mysql restart');
-shell_exec('cd /var/www/html; composer create-project drupal-composer/drupal-project:8.x-dev drupal --stability dev --no-interaction >> /root/drupal-rebuild.txt 2>&1');
-shell_exec("cd /var/www/html/drupal; /var/www/html/drupal/vendor/bin/drupal site:install standard --langcode=\"en\" --db-type=\"mysql\" --db-host=\"${db_host}\" --db-name=\"${db_name}\" --db-user=\"${db_user}\" --db-pass=\"${db_pass}\" --db-port=\"3306\" --site-mail=\"bjbrown@fsu.edu\" --account-name=\"admin\" --account-pass=\"${randpass}\" --account-mail=\"bjbrown@fsu.edu\" --no-interaction >> /root/drupal-rebuild.txt 2>&1");
+shell_exec('cd /var/www/html; composer create-project drupal-composer/drupal-project:8.x-dev drupal --stability dev --no-interaction >> /root/drupal.txt 2>&1');
+shell_exec("cd /var/www/html/drupal; /var/www/html/drupal/vendor/bin/drupal site:install standard --langcode=\"en\" --db-type=\"mysql\" --db-host=\"${db_host}\" --db-name=\"${db_name}\" --db-user=\"${db_user}\" --db-pass=\"${db_pass}\" --db-port=\"3306\" --site-mail=\"bjbrown@fsu.edu\" --account-name=\"admin\" --account-pass=\"${randpass}\" --account-mail=\"bjbrown@fsu.edu\" --no-interaction >> /root/drupal.txt 2>&1");
 shell_exec("echo ${randpass} > /root/u1p.txt");
