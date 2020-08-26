97a87d8c0af5da8935c668c1fe8a824e03eaf804
Bryan Brown <bryjbrown@gmail.com>
2020-08-26 17:48:35 -0400

    Whoopsie

--- build/drupal.config.sh
+++ build/drupal.config.sh
@@ -3,7 +3,7 @@ echo "Loading LDbase configurations..."
 cd /var/www/html/drupal/ >/dev/null 2>&1
 /var/www/html/drupal/vendor/bin/drush config-set "system.site" uuid "6d3939a8-a52f-4862-a77a-176786dcad2a" -y >> /root/drupal.config.txt 2>&1
 /var/www/html/drupal/vendor/bin/drush ev '\Drupal::entityManager()->getStorage("shortcut_set")->load("default")->delete();' >> /root/drupal.config.txt 2>&1
-/var/www/html/drupal/vendor/bin/drupal.config:import --directory=/var/www/html/drupal/web/libraries/ldbase_config/sync >> /root/drupal-config.txt 2>&1
+/var/www/html/drupal/vendor/bin/drupal config:import --directory=/var/www/html/drupal/web/libraries/ldbase_config/sync >> /root/drupal.config.txt 2>&1
 /var/www/html/drupal/vendor/bin/drupal ldbase:importTerms >> /root/drupal.config.txt 2>&1
 
 service apache2 restart >> /root/drupal.config.txt 2>&1
