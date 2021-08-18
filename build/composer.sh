cb3486a1a4e6db6804723707f2e08545825808e9
Brian Arsenault <barsenault@fsu.edu>
2021-08-18 13:44:52 -0400

    rollback upgrade from 2.1.5

--- build/composer.sh
+++ build/composer.sh
@@ -1,6 +1,6 @@
 cd /root
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-php composer-setup.php --version=2.1.5 --filename=composer --install-dir=/usr/local/bin
+php composer-setup.php --version=1.10.10 --filename=composer --install-dir=/usr/local/bin
 php -r "unlink('composer-setup.php');"
 chmod +x /usr/local/bin/composer
 /usr/local/bin/composer global require zaporylie/composer-drupal-optimizations
