00874d0bb8f650f66cdd2e1a70c71c880d7ec356
Bryan J. Brown <bjbrown@fsu.edu>
2024-01-25 00:37:07 -0600

    Update ldbase.102.sh

--- scripts/ldbase.102.sh
+++ scripts/ldbase.102.sh
@@ -2,18 +2,14 @@ cd /var/www/html/drupal
 /usr/local/bin/composer self-update
 /usr/local/bin/composer require "composer/installers:^1" --no-update
 /usr/local/bin/composer require "drush/drush:^12" --no-update
-/usr/local/bin/composer require "drupal/address:^2" --no-update
 /usr/local/bin/composer require "drupal/captcha:^2" --no-update
 /usr/local/bin/composer require "drupal/ctools:^4" --no-update
 /usr/local/bin/composer require "drupal/facets:^3" --no-update
-/usr/local/bin/composer require "drupal/group:^2" --no-update
 /usr/local/bin/composer require "drupal/health_check:^3" --no-update
 /usr/local/bin/composer require "drupal/metatag:^2" --no-update
+#/usr/local/bin/composer require "drupal/address:^2" --no-update 
+#/usr/local/bin/composer require "drupal/group:^2" --no-update
+#/usr/local/bin/composer require "drupal/group:^3" --no-update #Must update to ^2 before moving to ^3
 /usr/local/bin/composer update --with-all-dependencies
 /var/www/html/drupal/vendor/bin/drush updatedb
-/var/www/html/drupal/vendor/bin/drush cache:rebuild 
-
-/usr/local/bin/composer require "drupal/group:^3" --no-update
-/usr/local/bin/composer update --with-all-dependencies
-/var/www/html/drupal/vendor/bin/drush updatedb
-/var/www/html/drupal/vendor/bin/drush cache:rebuild 
+/var/www/html/drupal/vendor/bin/drush cache:rebuild
