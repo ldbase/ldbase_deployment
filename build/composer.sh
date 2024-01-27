cf04640cf54c8a1ac59075bd9bdf83adf5850244
Bryan J. Brown <bjbrown@fsu.edu>
2024-01-27 00:58:12 -0600

    Sets composer version to 2.6.6

--- build/composer.sh
+++ build/composer.sh
@@ -1,6 +1,8 @@
+echo "COMPOSER_ALLOW_SUPERUSER=1" >> /etc/environment
+source /etc/environment
 cd /root
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-php composer-setup.php --version=2.1.11 --filename=composer --install-dir=/usr/local/bin
+php composer-setup.php --version=2.6.6 --filename=composer --install-dir=/usr/local/bin
 php -r "unlink('composer-setup.php');"
 chmod +x /usr/local/bin/composer
 /usr/local/bin/composer --no-interaction global require zaporylie/composer-drupal-optimizations
