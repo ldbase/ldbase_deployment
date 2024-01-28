36edd1a19e5ac0bb1797f9cd7922464058953d82
Bryan J. Brown <bjbrown@fsu.edu>
2024-01-28 00:22:12 -0600

    Upgrades php.ini location and composer version

--- build/composer.sh
+++ build/composer.sh
@@ -5,5 +5,6 @@ php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 php composer-setup.php --version=2.6.6 --filename=composer --install-dir=/usr/local/bin
 php -r "unlink('composer-setup.php');"
 chmod +x /usr/local/bin/composer
+/usr/local/bin/composer global config --no-plugins allow-plugins.zaporylie/composer-drupal-optimizations true
 /usr/local/bin/composer --no-interaction global require zaporylie/composer-drupal-optimizations
 /usr/local/bin/composer --no-interaction config --global --auth github-oauth.github.com $LDBASE_GITHUB_TOKEN
