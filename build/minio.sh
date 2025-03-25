4342d7bd66f450859a55179593eba2ac4cb14c46
Bryan J. Brown <bjbrown@fsu.edu>
2025-03-25 07:03:57 -0500

    Update minio.sh

--- build/minio.sh
+++ build/minio.sh
@@ -8,7 +8,8 @@ wget https://dl.min.io/client/mc/release/linux-amd64/mc > /dev/null 2>&1
 chmod +x mc
 mv mc /usr/local/bin/
 
-minio server /data >> minio.log &
+mkdir -p /var/log/fsulib
+minio server /data >> /var/log/fsulib/ldbase_minio.log &
 mkdir /root/.mc; cp /ldbase_deployment/assets/mc.config.json /root/.mc/config.json
 mc mb "localhost/${S3_BUCKET_NAME}"
 mc policy set public "localhost/${S3_BUCKET_NAME}"
